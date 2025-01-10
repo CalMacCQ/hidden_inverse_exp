222692b7a28eec04104c89336fc8ec1aae42ea0a
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2025-01-10 16:41:33 +0000

    fix up __init__ file

--- src/hidden_inverse_exp/__init__.py
+++ src/hidden_inverse_exp/__init__.py
@@ -1,2 +1,20 @@
-def hello() -> str:
-    return "Hello from hidden-inverse-exp!"
+from .alternating_pass import alternating_cnots_pass
+
+from .gadget_pass import (
+    single_pauli_gadget_hi_pass,
+    general_pauli_gadget_hi_pass,
+)
+
+from .utils.circuit_builders import get_pauli_gadget, get_phase_gadget
+
+from .hseries import get_hidden_inverse_circuits_1q, get_hidden_inverse_circuits_2q
+
+__all__ = [
+    "alternating_cnots_pass",
+    "single_pauli_gadget_hi_pass",
+    "general_pauli_gadget_hi_pass",
+    "get_hidden_inverse_circuits_1q",
+    "get_hidden_inverse_circuits_2q",
+    "get_pauli_gadget",
+    "get_phase_gadget",
+]
